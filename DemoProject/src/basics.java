4040e50ad3845cc15551d1bec52013f58f56c4fd
Rizwan Ali Khowaja <rizcubeservices@gmail.com>
2020-05-01 18:08:31 +0100

    basic.java includes given, when, then and it asserts statusCode, Scope & Server-header

--- DemoProject/src/basics.java
+++ DemoProject/src/basics.java
@@ -1,13 +1,18 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
-
+import static org.hamcrest.Matchers.*;
 public class basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
+		// Validate if Add Place API is working as expected.
+		// given - all input details
+		// when  - Submit the API
+		// Then  - validate the response
+		
+		
 		RestAssured.baseURI = "https://rahulshettyacademy.com/";
-		given().queryParam("key", "qaclick").header("Content-Type","application/json")
+		given().log().all().queryParam("key", "qaclick").header("Content-Type","application/json")
 		.body("{\n" + 
 				"  \"location\": {\n" + 
 				"    \"lat\": -38.383494,\n" + 
@@ -24,10 +29,8 @@ public class basics {
 				"  \"website\": \"http://google.com\",\n" + 
 				"  \"language\": \"French-IN\"\n" + 
 				"}").when().post("maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		.then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
+		.header("Server", "Apache/2.4.18 (Ubuntu)");
 		
-		
-
 	}
-
 }
