7eb8f42bbaeea35df02cf95f2b7dca33105508b9
Rizwan Ali Khowaja <rizcubeservices@gmail.com>
2020-05-01 18:56:50 +0100

    moved the body data into a new package and new file payload.java - first test still passes

--- DemoProject/src/basics.java
+++ DemoProject/src/basics.java
@@ -1,6 +1,8 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import files.payload;
 public class basics {
 
 	public static void main(String[] args) {
@@ -8,29 +10,17 @@ public class basics {
 		// Validate if Add Place API is working as expected.
 		// given - all input details
 		// when  - Submit the API
-		// Then  - validate the response
-		
-		
+		// Then  - validate the response		
 		RestAssured.baseURI = "https://rahulshettyacademy.com/";
 		given().log().all().queryParam("key", "qaclick").header("Content-Type","application/json")
-		.body("{\n" + 
-				"  \"location\": {\n" + 
-				"    \"lat\": -38.383494,\n" + 
-				"    \"lng\": 33.427362\n" + 
-				"  },\n" + 
-				"  \"accuracy\": 50,\n" + 
-				"  \"name\": \"rizcube house\",\n" + 
-				"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
-				"  \"address\": \"Spiritual Landing place\",\n" + 
-				"  \"types\": [\n" + 
-				"    \"shoe park\",\n" + 
-				"    \"shop\"\n" + 
-				"  ],\n" + 
-				"  \"website\": \"http://google.com\",\n" + 
-				"  \"language\": \"French-IN\"\n" + 
-				"}").when().post("maps/api/place/add/json")
+		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope",equalTo("APP"))
 		.header("Server", "Apache/2.4.18 (Ubuntu)");
+		
+		// Add place > update Place with address 
+		// Get Place to validate if New address is present in response
+		
+		
 		
 	}
 }
---
+++ DemoProject/src/files/payload.java
@@ -0,0 +1,27 @@
+package files;
+
+public class payload {
+
+	public static String AddPlace()
+	{
+		return "{\n" + 
+				"  \"location\": {\n" + 
+				"    \"lat\": -38.383494,\n" + 
+				"    \"lng\": 33.427362\n" + 
+				"  },\n" + 
+				"  \"accuracy\": 50,\n" + 
+				"  \"name\": \"rizcube house\",\n" + 
+				"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
+				"  \"address\": \"Spiritual Landing place\",\n" + 
+				"  \"types\": [\n" + 
+				"    \"shoe park\",\n" + 
+				"    \"shop\"\n" + 
+				"  ],\n" + 
+				"  \"website\": \"http://google.com\",\n" + 
+				"  \"language\": \"French-IN\"\n" + 
+				"}";
+		
+	}
+
+
+}
