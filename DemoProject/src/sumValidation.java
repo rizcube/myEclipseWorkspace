65b3e6458b6752b2bde9c938850e736a723f1d11
rizcube <rizcubeservices@gmail.com>
2020-05-07 21:45:10 +0100

    installed testng, jcommander, guice to get TestNG to work

---
+++ DemoProject/src/sumValidation.java
@@ -0,0 +1,27 @@
+// https://testng.org/doc/download.html
+// https://repo1.maven.org/maven2/com/beust/jcommander/1.78/
+//https://github.com/google/guice/wiki/Guice422
+
+import org.testng.annotations.Test;
+
+import files.payload;
+import io.restassured.path.json.JsonPath;
+
+public class sumValidation {
+
+	@Test
+	public void sumOfcourses()
+	{
+		JsonPath js = new JsonPath(payload.CoursePrice());
+		int count = js.getInt("courses.size()");
+		
+		for (int i=0; i<count; i++) 
+		{
+			int price = js.getInt("courses["+i+"].price");
+			int copies = js.getInt("courses["+i+"].copies");
+			int amount = price * copies;
+			System.out.println(amount);
+		}
+	}
+	
+}
