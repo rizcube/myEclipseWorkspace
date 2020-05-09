1ed7f9f845d1a79a0558a8b428a27b3ff62bc017
rizcube <rizcubeservices@gmail.com>
2020-05-09 14:40:23 +0100

    added dataProvider notation,object getData method, and a two dimensional array to pass isbn, isle number

--- DemoProject/src/files/dynamicJson.java
+++ DemoProject/src/files/dynamicJson.java
@@ -1,5 +1,6 @@
 // Library API - completed in Postman now implementing it here
 package files;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
@@ -8,11 +9,11 @@ import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 public class dynamicJson {
 	
-	@Test
-	public void addbook() 
+	@Test(dataProvider = "BooksData")
+	public void addbook(String isbn, String aisle) 
 	{
 		RestAssured.baseURI = "http://216.10.245.166";
-		String response =given().body(payload.AddBook("asdf","adf")).
+		String response =given().body(payload.AddBook(isbn, aisle)).
 		when().post("/Library/Addbook.php")
 		.then().assertThat().statusCode(200)
 		.extract().response().asString();
@@ -22,7 +23,30 @@ public class dynamicJson {
 		//JsonPath js1 = new JsonPath(response);
 		//System.out.println(js1.getString("ID"));
 		String id =  js.getString("ID");
-		System.out.println(id);
-		
+		System.out.println(id);	
 	}	
+
+@DataProvider(name = "BooksData")  // using data provider annotation
+public Object[][] getData() 
+{
+	// array is the collection of elements Example! new object [] {2,3,4,5}
+	// multidimensional is collection of arrays.
+	return new Object[][] {{"Book1","0001"}, {"Book2","0002"}, {"Book3","0003"}, {"Book4","0004"}};
 }
+
+
+
+
+
+
+}
+
+
+// dynamically build json payload with external data inputs
+// Parameterize the API test with multiple data sets
+// How to send static Json files(payload) directly into Post Method of Rest Assured
+// Feed Json payload from using excel usign HashMap
+
+
+
+
