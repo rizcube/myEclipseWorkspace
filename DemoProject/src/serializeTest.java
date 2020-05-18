ee6d04729020fce2f52b9ba23fc9cb6480064934
rizcube <rizcubeservices@gmail.com>
2020-05-18 15:52:45 +0100

    created getters/setter based on the json data, passed all data to SeralizeTest main class

--- DemoProject/src/serializeTest.java
+++ DemoProject/src/serializeTest.java
@@ -1,35 +1,47 @@
 import io.restassured.RestAssured;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import okhttp3.Response;
+import pojo.AddPlace;
+import pojo.Location;
 
 import static io.restassured.RestAssured.*;
+
+import java.util.ArrayList;
+import java.util.List;
 public class serializeTest {
 
 	public static void main(String args[]) {
 		
 	RestAssured.baseURI = "https://rahulshettyacademy.com";
 			
-	String response = given().queryParam("key", "qaclick123").body("{\n" + 
-			"  \"location\": {\n" + 
-			"    \"lat\": -38.383494,\n" + 
-			"    \"lng\": 33.427362\n" + 
-			"  },\n" + 
-			"  \"accuracy\": 50,\n" + 
-			"  \"name\": \"rizcube Castle\",\n" + 
-			"  \"phone_number\": \"(+91) 983 893 3937\",\n" + 
-			"  \"address\": \"Spiritual Landing place\",\n" + 
-			"  \"types\": [\n" + 
-			"    \"shoe park\",\n" + 
-			"    \"shop\"\n" + 
-			"  ],\n" + 
-			"  \"website\": \"http://google.com\",\n" + 
-			"  \"language\": \"French-IN\"\n" + 
-			"}")
-	.when().post("/maps/api/place/add/json")
-	.then().assertThat().statusCode(200)
-	.extract().response().asString();
+	AddPlace p = new AddPlace();
+	p.setAccuracy(50);
+	p.setAddress("Spiritual Landing place");
+	p.setLanguage("French-IN");
+	p.setPhone_number("(+91) 983 893 3937");
+	p.setName("rizcube Castle");
+	p.setWebsite("http://google.com");
 	
-	System.out.println(response);
+	List<String> myList = new ArrayList<String>(); 
+	myList.add("shoe park");
+	myList.add("shop");
+	p.setTypes(myList);
+	
+	Location l = new Location();
+	l.setLat(-38.383494);
+	l.setLng(33.427362);
+	
+	p.setLocation(l);
+	
+	io.restassured.response.Response res = given().log().all().queryParam("key", "qaclick123").body(p)
+	.when().post("/maps/api/place/add/json")
+	.then().assertThat().statusCode(200).extract().response();
+	
+	String responseString = res.asString();
+	System.out.println(responseString);
+	
+	
 	
 	}
 }
