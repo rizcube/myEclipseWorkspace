914664584f3402b49c97a8076f29e8a3ddd01fe2
rizcube <rizcubeservices@gmail.com>
2020-05-18 11:44:03 +0100

    created a list variable called a and added course titles and created expectedList using for loop and then finally compared using Assert.assertTrue(a.equals(expectedList));

--- DemoProject/src/demo_Oauth/oAuthTest.java
+++ DemoProject/src/demo_Oauth/oAuthTest.java
@@ -2,6 +2,8 @@
 package demo_Oauth;
 import static io.restassured.RestAssured.given;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import io.restassured.parsing.Parser;
@@ -12,6 +14,7 @@ import pojo.webAutomation;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.testng.Assert;
 /* 
 https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.email&auth_url=https://accounts.google.com/o/oauth2/v2/auth 
 &client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com
@@ -24,21 +27,9 @@ public class oAuthTest {
 	
 	
 	public static void main(String[] args) throws InterruptedException {		
-		// initialize the chrome browser
-		/*System.setProperty("webdriver.chrome.driver", "/usr/local/bin");
-		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://accounts.google.com/signin/oauth/identifier?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&auth_url=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fv2%2Fauth&client_id=692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Frahulshettyacademy.com%2FgetCourse.php&state=abcd&o2v=2&as=JWUmfoXrFOUpQylg4jlJTQ&flowName=GeneralOAuthFlow");
-		driver.findElementByCssSelector("input[type='email']").sendKeys("rizcube");
-		driver.findElementByCssSelector("input[type='email']").sendKeys(Keys.ENTER);
-		// tagname[attribute ='value']
-		Thread.sleep(3000);
-		driver.findElementByCssSelector("input[type='password']").sendKeys("1EeshalAnabia100");
-		driver.findElementByCssSelector("input[type='password']").sendKeys(Keys.ENTER);
-		Thread.sleep(40000);
-		String url = driver.getCurrentUrl();
-		*/
+		String[] courseTitles = {"Selenium Webdriver Java","Cypress","Protractor"};
 		
-		String url ="https://rahulshettyacademy.com/getCourse.php?state=abcd&code=4%2FzwGXoMqrpzmg8O416PnEY26yfd6di-ucn1kc5Y9wEs9iMJXhStSwx4YfA83-U7SvXnYS1L2p6wEz5oFaSWuTO_U&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none#";
+		String url ="https://rahulshettyacademy.com/getCourse.php?state=abcd&code=4%2FzwGvdOF1csnfV-dz9gZ5qZIM2upgOn2W3c4AZK43d1WGzu7Q__FzUlSQIfNN7oOIacV57Il0xikh9m8ieSZZV14&scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=none#";
 		
 		String partialcode = url.split("code=")[1];
 		String code = partialcode.split("&scope")[0];
@@ -88,17 +79,22 @@ public class oAuthTest {
 		
 		}
 	
-		System.out.println(gc.getCourses().getWebAutomation().get(1).getCourseTitle());
-		
+		System.out.println(gc.getCourses().getWebAutomation().get(1).getCourseTitle());	
 		System.out.println("Query 3 Get all courses under webAutomationCourses");
+		
+		ArrayList<String> a = new ArrayList<String>();
 		
 		List<webAutomation> webAutomationCourses = gc.getCourses().getWebAutomation();
 		
 		System.out.println(webAutomationCourses.size());
 		
 		for (int i = 0; i< webAutomationCourses.size(); i++) {
-			System.out.println(webAutomationCourses.get(i).getCourseTitle());
+		//	System.out.println(webAutomationCourses.get(i).getCourseTitle());
+		a.add(webAutomationCourses.get(i).getCourseTitle());
 		}
-				
+		System.out.println(a);
+		List<String> expectedList = Arrays.asList(courseTitles);
+		Assert.assertTrue(a.equals(expectedList));
+		System.out.println(expectedList);
 	}
 }
